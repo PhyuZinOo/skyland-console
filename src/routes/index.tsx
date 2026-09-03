b0be1622271f198eb136b2ef66f64d54b82fefc8
Paingthuyakyaw <kyawna2265@gmail.com>
2026-09-03 15:49:58 +0630

    feat: inddx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createFileRoute } from "@tanstack/react-router"
 
-
 export const Route = createFileRoute("/")({
   component: HomePage,
 })
@@ -10,8 +9,7 @@ function HomePage() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <h1 className="font-medium">Project ready!</h1>  
-        <p>This project is ready to use.</p> 
+        <h1 className="font-medium">Project ready!</h1>   
       </div>
     </div>
   )
